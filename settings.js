1175efcaa8a6f9fda1aaecd368e66b2521361a63
idhafio <121158740+idhafio@users.noreply.github.com>
2022-12-22 19:55:23 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ global.thumb = fs.readFileSync(`./image/thumb.png`)
 global.qrisdonate = fs.readFileSync(`./image/qris.jpg`)
 global.fake = `📍𝑪𝒓𝒆𝒂𝒕𝒆𝒅 𝑩𝒚 𝑨𝗿𝗶 𝑩𝒐𝒕 𝑾𝒉𝒂𝒕𝒔𝑨𝒑𝒑`
 global.packname = ``
-global.author = `×͜× ▭▬▭▬▭▬ 👑 ▭▬▭▬▭▬ ×͜×\nﾒ 𝗖𝗿𝗲𝗮𝘁𝗲𝗱 𝗕𝘆 𝗞𝗮𝘆𝗹𝗮\nﾒ 𝗖𝗿𝗲𝗮𝘁𝗼𝗿 𝑨𝗿𝗶 : 085721920138\nﾒ ⊟————————⊟`
+global.author = `×͜× ▭▬▭▬▭▬ 👑 ▭▬▭▬▭▬ ×͜×\nﾒ 𝗖𝗿𝗲𝗮𝘁𝗼𝗿 𝑨𝗿𝗶 : 085721920138\nﾒ ⊟————————⊟`
 global.antilink = false
 global.antiwame = false
 global.autodltt = false
 global.autosticker = false
-global.ownerNomor = '6281348106274','6281251549899'
-global.ownerName = 'Creator DaniXd'
-global.ownerNumber = ["6281348106274@s.whatsapp.net","6281251549899@s.whatsapp.net"]
+global.ownerNomor = '6285721920138'
+global.ownerName = 'Creator Ari'
+global.ownerNumber = ["6285721920138@s.whatsapp.net"]
 global.cek1 = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100']
 global.mess = {
     wait: 'Wait Sis Please be patient',
@@ -36,7 +36,7 @@ global.bapak = [
 'Wah Mantap Lu Masih Punya Bapack\nPasti Bapack Nya Kuli :v\nAwowkwokwwok\n#CandabOs',
 'Aowkwwo Disini Ada Yteam :v\nLu Yteam Bro? Awowkwowk\nSabar Bro Ga Punya Bapack\n#Camda',
 'Bjir Bapack Mu Ternyata Sudah Cemrai\nSedih Bro Gua Liatnya\nTapi Nih Tapi :v\nTetep Ae Lu Yteam Aowkwowkw Ngakak :v',
-'Jangan #cekbapak Mulu Broo :v\nKasian Yang Yteam\nNtar Tersinggung Kan\nYahahaha Hayyuk By : Ramlan ID',
+'Jangan #cekbapak Mulu Broo :v\nKasian Yang Yteam\nNtar Tersinggung Kan\nYahahaha Hayyuk By : Ari ID',
 ]
 
 let file = require.resolve(__filename)
